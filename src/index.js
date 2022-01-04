fae12eb5c6f016ab0c913f88b7cef9553d028b18
Krajčí Peter <krajci@krajcimac.domain.local>
2022-01-04 16:15:27 +0100

    First project configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// Hallo :)
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Chellou");
+});
+
+//Connect to DB
+
+// Listen to server
+app.listen(3000);
+
+////node --exec node --experimental-specifier-resolution=node
