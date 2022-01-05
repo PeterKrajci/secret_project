3b9583ffe4052a59b046f1b141dc2796ff7d558d
Bukaiová Adriana <bukaiova@bukaiovamac.domain.local>
2022-01-05 16:35:34 +0100

    routes and services added

---
+++ src/routes/blogs.js
@@ -0,0 +1,10 @@
+import express from "express"
+import {param, validationResult} from "express-validator"
+
+import services from "../services"
+
+const router = express.Router()
+
+
+
+export default router
