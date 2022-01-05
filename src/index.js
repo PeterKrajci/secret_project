f6d539e4a2c1084140c82c0ed55da0ce0482410e
Bukaiová Adriana <bukaiova@bukaiovamac.domain.local>
2022-01-05 10:24:34 +0100

    DB connected

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+const { config } = require("dotenv");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require('path');
+require('dotenv').config({path: path.resolve('src', './.env')})
 
 //Routes
 app.get("/", (req, res) => {
@@ -8,6 +11,10 @@ app.get("/", (req, res) => {
 });
 
 //Connect to DB
+console.log(process.env.DB_CONNECTION);
+mongoose.connect(process.env.DB_CONNECTION,
+  {useNewUrlParser: true}, 
+  () => console.log('Connected to DB'));
 
 // Listen to server
 app.listen(3000);
--- src/models/blog.js
+++ src/models/blog.js
@@ -1,1 +1,37 @@
 //nieco
+/*const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://User1:<password>@test.fnhqc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});*/
+
+//mongodb+srv://User1:<password>@test.fnhqc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+
+
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    //comments
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    likes: Number,
+    //image
+})
