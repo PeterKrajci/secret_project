b757e5dcfb39afea5e50f97bc0feaf017428e4e2
Krajčí Peter <krajci@krajcimac.local>
2022-01-11 00:57:32 +0100

    added not tested route for getting blog by id

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -1,17 +1,22 @@
 import express from "express";
-import {param, validationResult} from "express-validator"
+import { param, validationResult } from "express-validator";
 
-import services from "../services"
-
+import services from "../services";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('We are on blogs');
+router.get("/", (req, res) => {
+  res.send("We are on blogs");
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.get(
+  "/:id",
+  param("id").isLength({ min: 24, max: 24 }),
+  services.blogs.getBlogById
+);
+
+router.post("/", (req, res) => {
+  console.log(req.body);
 });
 
 export default router;
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,3 +1,15 @@
 // add code here
+const getBlogById = async (req, res) => {
+  try {
+    const idOfBlog = req.params.id;
 
-export default {}
+    const blog = await Adventure.findById(idOfBlog).exec();
+
+    res.status(200).send(blog);
+  } catch (error) {
+    console.log(error);
+    res.send("Something went wrong!");
+  }
+};
+
+export default { getBlogById };
