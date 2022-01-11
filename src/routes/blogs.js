a5cc94dba6fc49ef2cd1afdae7c04360473e9803
AdkaB <86599981+AdkaB@users.noreply.github.com>
2022-01-11 16:23:33 +0100

    Inserting blog to db (#2)
    * trying branching
    
    * function_for_inserting_blogs_added
    
    * adding validation for inserting blogs
    
    * code_review_fixed
    
    Co-authored-by: Bukaiová Adriana <bukaiova@bukaiovamac.local>

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { param } from "express-validator";
+import { param, body } from "express-validator";
 
 import services from "../services";
 
 const router = express.Router();
+
+router.post(
+  "/",
+  body("title")
+    .isLength({ min: 3, max: 80 })
+    .withMessage(
+      "The length of the title must be min 3 and max 80 characters."
+    ),
+  services.blogs.addBlog)
 
 router.get("/", services.blogs.getAllBlogs);
 
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,5 +1,35 @@
-import { blogModel } from "../models/blog";
 import { validationResult } from "express-validator";
+import { blogModel } from "../models/blog";
+
+//inserting new blog to DB
+
+const addBlog = async (req, res) => {
+  try {
+    const validationResults = validationResult(req);
+
+    if (validationResults.isEmpty()) {
+      const blog = new blogModel({
+        title: req.body.title,
+        author: req.body.author,
+        text: req.body.text,
+      });
+
+      await blog.save();
+
+      res.status(200);
+      res.json(blog);
+    } else {
+      req.log.info(`validation error value: ${validationResults}`);
+      res.status(400);
+      res.send("validationError");
+      res.send(validationResults);
+    }
+  } catch (error) {
+    req.log.error(error);
+    res.sendStatus(500);
+    res.send("Error!");
+  }
+};
 
 // Function find all blogs in collection and send them as response
 const getAllBlogs = async (req, res) => {
@@ -49,4 +79,4 @@ const deleteBlogById = async (req, res) => {
   }
 };
 
-export default { deleteBlogById, getAllBlogs };
+export default { deleteBlogById, getAllBlogs, addBlog };
