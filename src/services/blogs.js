5898b87536caeeb69ad988cff44ad146d78d8541
Bukaiová Adriana <bukaiova@bukaiovamac.local>
2022-01-11 15:46:36 +0100

    code_review_fixed

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -6,24 +6,28 @@ import { param, validationResult } from "express-validator";
 const addBlog = async (req, res) => {
   try {
     const validationResults = validationResult(req);
+
     if (validationResults.isEmpty()) {
       const blog = new Blog({
         title: req.body.title,
         author: req.body.author,
         text: req.body.text,
       });
+
       await blog.save();
+
       res.status(200);
       res.json(blog);
     } else {
       req.log.info(`validation error value: ${validationResults}`);
       res.status(400);
       res.send("validationError");
+      res.send(validationResults);
     }
   } catch (error) {
+    req.log.error(error);
     res.sendStatus(500);
     res.send("Error!");
-    console.log(error);
   }
 };
 
