63d3d66df9437c8ac2fc54b92994afb88b0a7e32
ArsenDriashkaba <83950642+ArsenDriashkaba@users.noreply.github.com>
2022-01-11 15:56:32 +0100

    Deleting blog from db and getting list of all blogs (#1)
    * Base version of DeleteBlogById-Route
    
    * Implementing GettingListOfAllBlogs and adding validation for deletingBlogByID
    
    * Fixing bugs and code refactoring
    
    * Refactoring

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,3 +1,52 @@
-// add code here
+import { blogModel } from "../models/blog";
+import { validationResult } from "express-validator";
 
-export default {}
+// Function find all blogs in collection and send them as response
+const getAllBlogs = async (req, res) => {
+  try {
+    const allBlogs = await blogModel.find({});
+
+    req.log.info("Success");
+    res.status(200).send(allBlogs);
+  } catch (error) {
+    req.log.error(error);
+    res.sendStatus(500);
+  }
+};
+
+// Function find and delete blog from collection by id
+const deleteBlogById = async (req, res) => {
+  try {
+    const validationResults = validationResult(req);
+    const idOfBlog = req.params.id.toString().trim();
+
+    if (!validationResults.isEmpty()) {
+      req.log.error(validationResults);
+      res.status(400).send(validationResults);
+
+      return;
+    }
+
+    // trying to delete blog from collection
+    const deletedBlog = await blogModel.findByIdAndRemove(idOfBlog).exec();
+
+    if (deletedBlog == null) {
+      const blogNotFoundMsg = `Blog with id "${idOfBlog}" is not found...`;
+
+      req.log.error(blogNotFoundMsg);
+      res.status(404).send(blogNotFoundMsg);
+
+      return;
+    }
+
+    const successMsg = `You've succsesfully deleted blog with id: ${req.params.id}`;
+
+    req.log.info(successMsg);
+    res.status(200).send(successMsg);
+  } catch (error) {
+    req.log.error(error);
+    res.sendStatus(500);
+  }
+};
+
+export default { deleteBlogById, getAllBlogs };
