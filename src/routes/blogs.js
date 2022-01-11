8cf0c971f8b4a8fc9dba2b7451fb22d2995fca2e
Bukaiová Adriana <bukaiova@bukaiovamac.local>
2022-01-11 11:18:50 +0100

    adding validation for inserting blogs

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { param, validationResult } from "express-validator";
+import { param, body, validationResult } from "express-validator";
 
 import services from "../services";
 
 const router = express.Router();
 
-router.post("/", services.blogs.addBlog);
+router.post(
+  "/",
+  body("title")
+    .isLength({ min: 3, max: 80 })
+    .withMessage(
+      "The length of the title must be min 3 and max 80 characters."
+    ),
+  services.blogs.addBlog
+);
 
 export default router;
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,20 +1,25 @@
 import Blog from "../models/blog";
 import { param, validationResult } from "express-validator";
 
-//adding new blog
+//inserting new blog to DB
 
 const addBlog = async (req, res) => {
   try {
-    const blog = new Blog({
-      title: req.body.title,
-      author: req.body.author,
-      text: req.body.text,
-    });
-
-    await blog.save();
-
-    res.status(200);
-    res.json(blog);
+    const validationResults = validationResult(req);
+    if (validationResults.isEmpty()) {
+      const blog = new Blog({
+        title: req.body.title,
+        author: req.body.author,
+        text: req.body.text,
+      });
+      await blog.save();
+      res.status(200);
+      res.json(blog);
+    } else {
+      req.log.info(`validation error value: ${validationResults}`);
+      res.status(400);
+      res.send("validationError");
+    }
   } catch (error) {
     res.sendStatus(500);
     res.send("Error!");
@@ -23,9 +28,3 @@ const addBlog = async (req, res) => {
 };
 
 export default { addBlog };
-
-// const blog = await Blog.create({
-//   title: req.body.title,
-//   author: req.body.author,
-//   text: req.body.text,
-// req.log.error(error)
